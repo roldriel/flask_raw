6f074b32872d86972f92dfb49461030bb552a6af
Rodrigo Roldán <roldriel@gmail.com>
2024-11-08 21:01:36 -0300

    Update requirements, update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,16 @@
 import pytest
-from src.app import create_app  # Asumiendo que el archivo se llama app.py y está en src/
+from src.app import create_app
 
 @pytest.fixture
 def client():
-    app = create_app()  # Crear la app usando el factory
-    app.config['TESTING'] = True  # Activar modo de pruebas
+    """Client test function"""
+    app = create_app()
+    app.config['TESTING'] = True
     with app.test_client() as client:
         yield client
 
 def test_home(client):
+    """Test home endpoint"""
     response = client.get('/')
     assert response.status_code == 200
-    assert b'Hello, World!!' in response.data  # O cualquier contenido esperado
+    assert b'Hello, World!!' in response.data
