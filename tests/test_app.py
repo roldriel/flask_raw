c5b551b50a8d6d4f23e581510306ca5e439319c9
Rodrigo Roldán <roldriel@gmail.com>
2024-11-08 21:06:54 -0300

    Changes for Pytest

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,4 @@
+"""Tests endpoints module"""
 import pytest
 from src.app import create_app
 
@@ -9,8 +10,8 @@ def client():
     with app.test_client() as client:
         yield client
 
-def test_home(client):
+def test_home(test_client):
     """Test home endpoint"""
-    response = client.get('/')
+    response = test_client.get('/')
     assert response.status_code == 200
     assert b'Hello, World!!' in response.data
